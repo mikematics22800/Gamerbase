07ca5aad61bd7d05831f35f0360b1e29a5c103df
Jahdona <jahdonah@yahoo.com>
2024-09-25 20:48:49 -0700

    Fixing the footer on the bottom of the page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,6 +4,16 @@ const AppFooter = () => {
   
   return (
     <>
+    <style jsx="true">
+    {`
+      .footer{
+      width:100%;
+      position:fixed;
+      bottom:0;
+      }
+    `}
+    </style>
+    <div className='footer'>
       <Navbar className='bg-success text-white' variant='blue' expand='lg'>
         <Container fluid>
         
@@ -11,15 +21,15 @@ const AppFooter = () => {
          
           <Navbar.Toggle aria-controls='navbar' />
           <Navbar.Collapse id='navbar' className='d-flex justify-content-around'>
-          <h3 className='text-white'>Developed  by Group 7<h6><br/>Website created for educational purposes and uses API data from RAWG Video Game Database https://rawg.io/</h6></h3>
-          <h3 > </h3> 
-          <h3>Contributors : 🧡Dave 🧡Michael 🧡Daved 🧡Jean 🧡Rashawn</h3>
+          <h4 className='text-white'>Developed  by Group 7</h4>
+          <h6><br/>Website created for educational purposes and<br/> uses API data from RAWG Video Game Database https://rawg.io/</h6>
+          <h4>Contributors : 🧡Dave 🧡Michael 🧡Daved 🧡Jean 🧡Rashawn</h4>
          
          
           </Navbar.Collapse>
         </Container>
       </Navbar>
-    
+      </div>
       
     </>
   );
